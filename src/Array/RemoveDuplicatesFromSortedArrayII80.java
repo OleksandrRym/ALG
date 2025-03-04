e6935533b3f575ecf956da466af42c43e1fdd2c3
OleksandrRym <lostmannight@gmail.com>
2025-03-04 11:02:02 +0200

    commit task 80

---
+++ src/Array/RemoveDuplicatesFromSortedArrayII80.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class RemoveDuplicatesFromSortedArrayII80 {
+    public int removeDuplicates(int[] nums) {
+        int k=2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]!=nums[k-2]){
+                nums[k]=nums[i];
+                k++;
+            }
+
+        }
+        return k;
+
+    }
+}
